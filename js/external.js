87c94ef79b1badf123659f5d02c6683af6ea2fef
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-08-12 15:17:41 -0500

    Completed Exercise 1 and 2

--- js/external.js
+++ js/external.js
@@ -1,3 +1,9 @@
 "use strict";
 
 console.log("Hello from external JS!");
+
+alert("Welcome to my Website!");
+
+var favColor = prompt("What is your favorite color?");
+
+alert("Great, my favorite color is " + favColor + " too!");
