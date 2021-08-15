73a16f0b8f904018e144382e1d7df8527286a759
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-08-15 12:45:45 -0500

    Refactored/Analyzed more code

--- js/functions-weekend-practice.js
+++ js/functions-weekend-practice.js
@@ -110,6 +110,9 @@ why. So it's looking like functions can't do anything with variables outside of 
 because it can't calculate "cupcake" and "eat". But when I try putting the variables in a function...
  */
 
+
+
+
 function cupcakeCalc2() {
     var cupcake2 = prompt("How many cupcakes do you have?")
     var eat2 = prompt("How many cupcakes did you eat?")
@@ -125,6 +128,8 @@ UPDATE:
 Okay, the "cupcakeCalc2" code actually DOES work, but I forgot the () to call it. Whoops.
  */
 
+
+
 function cupcakeCalc3(c, e) {
     var cupcakesLeft3 = c - e;
     return cupcakesLeft3;
@@ -136,13 +141,47 @@ an attempt to figure out where I was going wrong with "cupcakeCalc2", but nothin
 "cupcakeCalc2" or "cupcakeCalc3"!
  */
 
+
+
 function cupcakeCalc4() {
     var cupcake4 = prompt("How many cupcakes do you have?")
     var eat4 = prompt("How many cupcakes did you eat?")
     var cupcakesLeft4 = cupcake4 - eat4;
     alert("You have " + cupcakesLeft4 + " cupcakes left!");
+    return cupcakesLeft4;
 }
 
 /* NOTE FROM ME:
 I added an alert so the user is notified how many cupcakes they have left!
+
+UPDATE:
+Now the console stores the result of the problem so we won't forget it!
  */
+
+
+
+function calcPaycheck () {
+
+    var googleResponse = prompt("How many hours did you work at Google this week?");
+    var amazonResponse = prompt("How many hours did you work at Amazon this week?");
+    var facebookResponse = prompt("How many hours did you work at Facebook this week?");
+
+
+    var google = 400 * googleResponse;
+    var amazon = 380 * amazonResponse;
+    var facebook = 350 * facebookResponse;
+
+    var answer = (google + facebook + amazon);
+
+    alert("You earned $" + answer + " dollars this week!");
+    return answer;
+
+}
+
+/* NOTE FROM ME:
+I refactored code from my "external.js" file and put it inside of a function. The code only
+executed when I called the function, which made the page less cluttered when I loaded it up.
+Originally, all these alerts would pop up at once, but it doesn't do that inside a function.
+I imagine functions could be useful when clicking a button, like entering a date. When the
+button is clicked, the function runs! Otherwise, it's dormant.
+*/
