af6077fe278f21421cf120511bfb4d27959e65ab
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-09-02 09:17:23 -0500

    Started JS Warmup

--- js/warmups.js
+++ js/warmups.js
@@ -42,3 +42,55 @@ function rollSixSided (num) {
 
 }
 
+ /*WARM UP DATE: 9-2-21
+
+
+ Why pay a fortune teller when you can just program your fortune yourself?
+
+    Write a function named tellFortune that:
+    takes 4 arguments: number of children, partner's name, geographic location, job title.
+
+
+Outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+
+
+Call that function 3 times with 3 different values for the arguments.
+
+*/
+
+/* NOTES FROM ME:
+
+This is the most basic way to do the exercise.
+
+ */
+
+
+function tellFortune(x, y, z, n) {
+    return "You will be a " + x + " in " + y + " and married to " + z + " with " + n + " kids."
+}
+
+console.log(tellFortune("artist", "new york", "shouta", 0))
+console.log(tellFortune("doctor", "texas", "bob", 10))
+console.log(tellFortune("warrior", "rome", "athena", 200))
+
+
+
+
+/* NOTES FROM ME:
+
+This is the more user-involved way to complete the exercise.
+
+ */
+
+function tellFortuneAdvanced() {
+
+    var job = prompt("What is your dream job?")
+    var location = prompt("Where do you want to live?")
+    var marry = prompt("Who would you like to marry?")
+    var kids = prompt("How many kids do you want?")
+
+    alert("You will be a " + job + " in " + location + " and married to " + marry + " with " + kids + " kids.");
+
+}
+
+tellFortuneAdvanced();
