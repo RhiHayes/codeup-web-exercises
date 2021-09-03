dc4bf4f26b3799c93acb7631a2fc1afed5a4d5ed
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-09-03 09:41:12 -0500

    Finished JS Exercise

--- js/warmups.js
+++ js/warmups.js
@@ -94,3 +94,44 @@ function tellFortuneAdvanced() {
 }
 
 tellFortuneAdvanced();
+
+/* WARM UP DATE: 9/3/21
+
+You know how old your dog is in human years, but what about dog years? Calculate it!
+    Write a function named calculateDogAge that:
+       * takes 1 argument: your puppy’s age.
+       * calculates your dog’s age based on the conversion rate of 1 human year to 7 dog years.
+       * outputs the result to the screen like so: “Your doggie is NN years old in dog years!”
+    Call the function three times with different sets of values.
+    Bonus: Add an additional parameter to the function that takes the conversion rate of human to dog years.
+
+ */
+
+
+/* NOTES FROM ME:
+
+This is the most basic way to do the exercise.
+
+ */
+
+
+function calculateDogAge(dog, human) {
+    var dogAge = dog * 7
+    console.log("Your dog is " + dogAge + " in dog years!")
+
+    /* BONUS */
+
+    var humanDogAge = human * 7
+    console.log("Your age converted to dog years is " + humanDogAge + " years old!")
+
+}
+
+console.log(calculateDogAge(1, 18))
+console.log(calculateDogAge(3, 21))
+console.log(calculateDogAge(5, 30))
+
+
+
+
+
+
