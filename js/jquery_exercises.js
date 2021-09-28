d8bb69770e929e1c9663be50b942301064655125
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-09-28 12:21:27 -0500

    Finished JQuery Selectors Exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -5,3 +5,26 @@ $(document).ready(function() {
     alert("Document ready- JS is ready to rock and roll!");
 
 });
+
+$("#title").css("font-size", "80px");
+alert("Title is now 80px.");
+
+$("#full-list").css("color", "blue");
+alert("Color of list is now blue.");
+
+
+$(".codeup").css("border", "1px solid red");
+
+
+
+$("li").css("font-size", "20px");
+
+// $("li").css("background-color", "yellow");
+
+// $("h1").css("background-color", "yellow");
+
+// $("p").css("background-color", "yellow");
+//alert("Title has a yellow background.");
+
+
+$("h1, li, p").css("background-color", "yellow");
