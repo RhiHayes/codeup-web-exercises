e56635909afa2f5ed7d26a9783c414f3ae11c63f
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-09-28 15:04:15 -0500

    Finished JQuery Mouse Events Exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,30 +1,48 @@
 "use strict";
+//
+// $(document).ready(function() {
+//
+//     alert("Document ready- JS is ready to rock and roll!");
+//
+// });
+//
+// $("#title").css("font-size", "80px");
+// alert("Title is now 80px.");
+//
+// $("#full-list").css("color", "blue");
+// alert("Color of list is now blue.");
+//
+//
+// $(".codeup").css("border", "1px solid red");
+//
+//
+//
+// $("li").css("font-size", "20px");
+//
+// // $("li").css("background-color", "yellow");
+//
+// // $("h1").css("background-color", "yellow");
+//
+// // $("p").css("background-color", "yellow");
+// //alert("Title has a yellow background.");
+//
+//
+// $("h1, li, p").css("background-color", "yellow");
 
-$(document).ready(function() {
+$("h1").on("click", function() {
+    $(this).css("background-color", "yellow")
+})
 
-    alert("Document ready- JS is ready to rock and roll!");
 
+$("p").on("dblclick", function() {
+    $(this).css("font-size", "18px")
+})
+
+
+$("li").hover(function() {
+    $(this).css("color", "red")
+},
+
+    function() {
+    $(this).css("color", "black")
 });
-
-$("#title").css("font-size", "80px");
-alert("Title is now 80px.");
-
-$("#full-list").css("color", "blue");
-alert("Color of list is now blue.");
-
-
-$(".codeup").css("border", "1px solid red");
-
-
-
-$("li").css("font-size", "20px");
-
-// $("li").css("background-color", "yellow");
-
-// $("h1").css("background-color", "yellow");
-
-// $("p").css("background-color", "yellow");
-//alert("Title has a yellow background.");
-
-
-$("h1, li, p").css("background-color", "yellow");
